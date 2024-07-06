94481f3d015dba82ff452e2170f4ede418b2cd54
Gustavo Pessa <ghmpessa@gmail.com>
2024-07-06 19:00:20 -0300

    feat: add initial email existence validation

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -14,6 +14,16 @@ export const register = async (req: FastifyRequest, res: FastifyReply) => {
 
   const password_hash = await hash(password, 6)
 
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    return res.status(409).send()
+  }
+
   await prisma.user.create({
     data: {
       name,
