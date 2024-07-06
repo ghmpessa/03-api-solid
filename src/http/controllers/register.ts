aece0a59d3475badaa0877606d5b770ffe738fdd
Gustavo Pessa <ghmpessa@gmail.com>
2024-07-06 19:11:03 -0300

    refactor: add registerUseCase

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,5 @@
+import { registerUseCase } from '@/usecases/register'
 import { FastifyRequest, FastifyReply } from 'fastify'
-import { hash } from 'bcryptjs'
-import { prisma } from '@/lib/prisma'
 import { z } from 'zod'
 
 export const register = async (req: FastifyRequest, res: FastifyReply) => {
@@ -12,25 +11,11 @@ export const register = async (req: FastifyRequest, res: FastifyReply) => {
 
   const { name, email, password } = registerBodySchema.parse(req.body)
 
-  const password_hash = await hash(password, 6)
-
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  })
-
-  if (userWithSameEmail) {
+  try {
+    await registerUseCase({ name, email, password })
+  } catch (error) {
     return res.status(409).send()
   }
-
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash,
-    },
-  })
 
   return res.status(201).send()
 }
