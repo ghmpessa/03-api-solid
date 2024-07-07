65b49e74b18c2aeca61c2b319278b5512d0b29f1
Gustavo Pessa <ghmpessa@gmail.com>
2024-07-07 01:56:12 -0300

    refactor: import UsersRepository from correct place

--- src/usecases/register.ts
+++ src/usecases/register.ts
@@ -1,4 +1,4 @@
-import { UsersRepository } from '@/repositories/prisma/users-repository'
+import { UsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
 import { UserAlreadyExistsError } from './errors/user-alerady-exists-error'
 import { User } from '@prisma/client'
