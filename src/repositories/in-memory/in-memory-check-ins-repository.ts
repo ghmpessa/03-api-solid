9c4a0c7d095868eb594b9ff30fe1312bf3885b8c
Gustavo Pessa <ghmpessa@gmail.com>
2024-07-12 19:44:26 -0300

    refactor: add pagination to findManyByUserId on InMemoryCheckInsRepository

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -37,7 +37,9 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     return checkIn
   }
 
-  async findManyByUserId(userId: string) {
-    return this.items.filter((item) => item.user_id === userId)
+  async findManyByUserId(userId: string, page: number) {
+    return this.items
+      .filter((item) => item.user_id === userId)
+      .slice((page - 1) * 20, page * 20)
   }
 }
