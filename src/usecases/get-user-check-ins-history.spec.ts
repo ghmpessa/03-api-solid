98de05c62318a873a46f4a163366d003c67ff45c
Gustavo Pessa <ghmpessa@gmail.com>
2024-07-12 19:33:47 -0300

    test: ensure GetUserCheckInsHistory returns check in history list

---
+++ src/usecases/get-user-check-ins-history.spec.ts
@@ -0,0 +1,42 @@
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+
+import { beforeEach, describe, expect, it, vi, afterEach } from 'vitest'
+import { GetUserCheckInsHistoryUseCase } from './get-user-check-ins-history'
+
+let checkInRepository: InMemoryCheckInsRepository
+let sut: GetUserCheckInsHistoryUseCase
+
+describe('Get user check in use case', () => {
+  beforeEach(() => {
+    checkInRepository = new InMemoryCheckInsRepository()
+    sut = new GetUserCheckInsHistoryUseCase(checkInRepository)
+
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
+  it('should be able get user check in history', async () => {
+    await checkInRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    })
+
+    await checkInRepository.create({
+      gym_id: 'gym-02',
+      user_id: 'user-01',
+    })
+
+    const { checkIns } = await sut.execute({
+      userId: 'user-01',
+    })
+
+    expect(checkIns).toHaveLength(2)
+    expect(checkIns).toEqual([
+      expect.objectContaining({ gym_id: 'gym-01' }),
+      expect.objectContaining({ gym_id: 'gym-02' }),
+    ])
+  })
+})
