7f402e9b1ad816d4c7b5c8cfeb77bbd7481ef6ef
Gustavo Pessa <ghmpessa@gmail.com>
2024-07-12 19:44:41 -0300

    test: ensure GetUserCheckInsHistoryUseCase returns paginated history

--- src/usecases/get-user-check-ins-history.spec.ts
+++ src/usecases/get-user-check-ins-history.spec.ts
@@ -31,12 +31,33 @@ describe('Get user check in use case', () => {
 
     const { checkIns } = await sut.execute({
       userId: 'user-01',
+      page: 1,
     })
 
     expect(checkIns).toHaveLength(2)
     expect(checkIns).toEqual([
       expect.objectContaining({ gym_id: 'gym-01' }),
       expect.objectContaining({ gym_id: 'gym-02' }),
+    ])
+  })
+
+  it('should be able get paginated user check in history', async () => {
+    for (let index = 1; index <= 22; index++) {
+      await checkInRepository.create({
+        gym_id: `gym-${index}`,
+        user_id: 'user-01',
+      })
+    }
+
+    const { checkIns } = await sut.execute({
+      userId: 'user-01',
+      page: 2,
+    })
+
+    expect(checkIns).toHaveLength(2)
+    expect(checkIns).toEqual([
+      expect.objectContaining({ gym_id: 'gym-21' }),
+      expect.objectContaining({ gym_id: 'gym-22' }),
     ])
   })
 })
