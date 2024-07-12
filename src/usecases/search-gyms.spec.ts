13599accae3014364cd6f84e80f41e4c4359779c
Gustavo Pessa <ghmpessa@gmail.com>
2024-07-12 20:23:12 -0300

    test: ensure SearchGymsUseCase returns paginated gyms

--- src/usecases/search-gyms.spec.ts
+++ src/usecases/search-gyms.spec.ts
@@ -47,4 +47,28 @@ describe('Search gyms use case', () => {
       expect.objectContaining({ title: 'Muscle Gym' }),
     ])
   })
+
+  it('should be able get paginated gyms', async () => {
+    for (let index = 1; index <= 22; index++) {
+      gymsRepository.create({
+        id: 'gym-01',
+        title: `Muscle Gym ${index}`,
+        description: null,
+        phone: null,
+        latitude: -22.2265344,
+        longitude: -45.9341824,
+      })
+    }
+
+    const { gyms } = await sut.execute({
+      query: 'Gym',
+      page: 2,
+    })
+
+    expect(gyms).toHaveLength(2)
+    expect(gyms).toEqual([
+      expect.objectContaining({ title: 'Muscle Gym 21' }),
+      expect.objectContaining({ title: 'Muscle Gym 22' }),
+    ])
+  })
 })
