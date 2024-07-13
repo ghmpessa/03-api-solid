8dfa1928682a00e2c6718536432a4baaf881c4a8
Gustavo Pessa <ghmpessa@gmail.com>
2024-07-13 14:02:18 -0300

    feat: add GetUserProfileUseCase factory

---
+++ src/usecases/factories/make-get-user-profile-use-case.ts
@@ -0,0 +1,8 @@
+import { GetUserProfileUseCase } from '../get-user-profile'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+
+export function makeGetUserProfileUseCase() {
+  const usersRepository = new PrismaUsersRepository()
+
+  return new GetUserProfileUseCase(usersRepository)
+}
