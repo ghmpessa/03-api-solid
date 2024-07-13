277eab5a96b4f8584e0f0c3c2ab5e98307355b3b
Gustavo Pessa <ghmpessa@gmail.com>
2024-07-13 13:58:34 -0300

    feat: add CheckInUseCase factory

---
+++ src/usecases/factories/make-check-in-use-case.ts
@@ -0,0 +1,10 @@
+import { CheckInUseCase } from '../check-in'
+import { PrismaGymsRepository } from '@/repositories/prisma/prisma-gyms-repository'
+import { PrismaCheckInsRepository } from '@/repositories/prisma/prisma-check-ins-repository'
+
+export function makeCheckInUseCase() {
+  const checkInsRepository = new PrismaCheckInsRepository()
+  const gymsRepository = new PrismaGymsRepository()
+
+  return new CheckInUseCase(checkInsRepository, gymsRepository)
+}
