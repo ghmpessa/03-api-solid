50c9c04edeaa611eb26875ed4e3888b81bfed900
Gustavo Pessa <ghmpessa@gmail.com>
2024-07-13 20:30:10 -0300

    refactor: return token from authenticate controller

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -14,7 +14,20 @@ export const authenticate = async (req: FastifyRequest, res: FastifyReply) => {
   try {
     const authenticateUseCase = makeAuthenticateUseCase()
 
-    await authenticateUseCase.execute({ email, password })
+    const { user } = await authenticateUseCase.execute({ email, password })
+
+    const token = await res.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      },
+    )
+
+    return res.status(200).send({
+      token,
+    })
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
       return res.status(400).send({ message: err.message })
@@ -22,6 +35,4 @@ export const authenticate = async (req: FastifyRequest, res: FastifyReply) => {
 
     throw err
   }
-
-  return res.status(200).send()
 }
