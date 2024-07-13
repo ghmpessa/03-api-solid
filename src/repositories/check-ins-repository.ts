51fc6fd01520a467962a97e984269f1be1cda78b
Gustavo Pessa <ghmpessa@gmail.com>
2024-07-12 21:09:28 -0300

    feat: add findById and save methods to CheckInsRepository

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -3,6 +3,8 @@ import { CheckIn, Prisma } from '@prisma/client'
 export interface CheckInsRepository {
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
+  findById(checkInId: string): Promise<CheckIn | null>
   countByUserId(userId: string): Promise<number>
   findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
+  save(checkIn: CheckIn): Promise<CheckIn>
 }
