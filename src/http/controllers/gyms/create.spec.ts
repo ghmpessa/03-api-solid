d87443843bbb85739b3aadc0540a30e0bc064354
Gustavo Pessa <ghmpessa@gmail.com>
2024-07-14 15:22:50 -0300

    refactor: add isAdmin param to avoid errors

--- src/http/controllers/gyms/create.spec.ts
+++ src/http/controllers/gyms/create.spec.ts
@@ -13,7 +13,7 @@ describe('Create gym E2E', () => {
   })
 
   it('should be able to create a gym', async () => {
-    const { token } = await createAndAuthenticateUser(app)
+    const { token } = await createAndAuthenticateUser(app, true)
 
     await request(app.server)
       .post('/gyms')
