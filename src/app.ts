07bf3078abe62eb0870c23278913832eb9260cc3
Gustavo Pessa <ghmpessa@gmail.com>
2024-07-14 00:38:49 -0300

    feat: add gym routes register

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { usersRoutes } from './http/controllers/users/routes'
 import { ZodError } from 'zod'
 import { env } from './env'
 import fastifyJwt from '@fastify/jwt'
+import { gymRoutes } from './http/controllers/gyms/routes'
 
 export const app = fastify()
 
@@ -11,6 +12,7 @@ app.register(fastifyJwt, {
 })
 
 app.register(usersRoutes)
+app.register(gymRoutes)
 
 app.setErrorHandler((error, _, res) => {
   if (error instanceof ZodError) {
