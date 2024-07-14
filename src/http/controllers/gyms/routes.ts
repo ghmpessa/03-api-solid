ce619378337ada52bbd9de8eb041aa9cacefd351
Gustavo Pessa <ghmpessa@gmail.com>
2024-07-14 00:38:42 -0300

    feat: add last gym routes

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,10 +1,15 @@
 import { FastifyInstance } from 'fastify'
 
 import { verifyJWT } from '../../middlewares/verify-jew'
+
 import { create } from './create'
+import { search } from './search'
+import { nearby } from './nearby'
 
 export const gymRoutes = async (app: FastifyInstance) => {
   app.addHook('onRequest', verifyJWT)
 
   app.post('/gyms', create)
+  app.get('/gyms/search', search)
+  app.get('/gyms/nearby', nearby)
 }
