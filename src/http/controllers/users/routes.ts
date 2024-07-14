7e59c168b27d3cece79d479cbb7749cbc025dc01
Gustavo Pessa <ghmpessa@gmail.com>
2024-07-14 15:03:16 -0300

    feat: add refresh token route

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,10 +3,13 @@ import { register } from './register'
 import { authenticate } from './authenticate'
 import { profile } from './profile'
 import { verifyJWT } from '../../middlewares/verify-jew'
+import { refresh } from './refresh'
 
 export const usersRoutes = async (app: FastifyInstance) => {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   app.get(
     '/me',
